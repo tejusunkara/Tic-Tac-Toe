e0b3465bf258f53e8fcd23600bc696933bf93581
tejusunkara <jnanatejaswini@gmail.com>
2021-03-14 14:39:59 +0000

    starting unmocked unit testing; created files and directory needed for it

---
+++ tests/unmocked/unit_tests.py
@@ -0,0 +1,12 @@
+import unittest
+import unittest.mock as mock
+from unittest.mock import patch
+import os
+import sys
+
+# This lets you import from the parent directory (one level up)
+sys.path.append(os.path.abspath('../'))
+
+KEY_INPUT = "input"
+KEY_EXPECTED = "expected"
+
